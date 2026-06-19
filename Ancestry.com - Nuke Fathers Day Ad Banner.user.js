3747594540519b98c47abcf848ec24b1afc3d5a5
Tim Conrad <tconrad63@gmail.com>
2026-06-19 15:24:23 -0400

    Add files via upload

---
+++ Ancestry.com - Nuke Fathers Day Ad Banner.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Ancestry.com - Nuke Ad Banner
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  Completely removes the ad banner and reclaims the full inch of space
+// @author       You
+// @match        https://www.ancestry.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    //const selector = '#WebOfferRecommendationSpot_us_subs_2026_mothers_day';
+    const selector = '#WebOfferRecommendationSpot_us_2026_fathers_day';
+
+    function nukeAd() {
+        const ad = document.querySelector(selector);
+        if (ad) {
+            // Completely delete the element
+            ad.remove();
+
+            // Fix any leftover space on parent containers
+            let parent = ad.parentElement;
+            while (parent && parent !== document.body) {
+                parent.style.setProperty('margin-top', '0', 'important');
+                parent.style.setProperty('padding-top', '0', 'important');
+                parent.style.setProperty('height', 'auto', 'important');
+                parent = parent.parentElement;
+            }
+
+            // Extra cleanup on body
+            document.body.style.setProperty('margin-top', '0', 'important');
+            document.documentElement.style.setProperty('padding-top', '0', 'important');
+
+            console.log('✅ Ancestry Mothers Day ad completely removed + space reclaimed');
+        }
+    }
+
+    // Run immediately and keep watching
+    nukeAd();
+
+    new MutationObserver(nukeAd).observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+
+    setInterval(nukeAd, 1000);
+})();
